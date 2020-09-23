b1b3d36881389ad2bb3b013fad68cef5cbf115d7
Parifly <parinita@flytrueair.com>
2020-09-22 17:56:50 -0700

    Changed config file with UserPoolId and UserPoolClientID

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-west-2_tNoTAoS7m', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7e2j2rnulvsdagklgqd0hh1onk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_zHHnOFhbE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14f6u6jteq27ntmdmfeppsic8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://8la7ginfpf.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
